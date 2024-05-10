7a35cd9e220b9eef1c3b9740ce8cacb60f52428d
Keith Nordstrom <keith@madsync.com>
2024-05-10 17:22:19 -0600

    Feature/86b09hx47 api for schedule (#10)
    * making email fetchable in batches
    
    * extended email model and test for extractor
    
    * makefile merge conflict
    
    * Adding a more complex email model with cross-references for search
    
    * Better gitignore
    
    * Better gitignore with gmail creds removed
    
    * coveragee file just makes things more painful, removing it
    
    * Reinstating a lost change to logic
    
    * Removing some prints
    
    * Adding parameter to specify a limit to context
    
    * Fixing the prompt and adding some prints for clarity
    
    * Fixing print
    
    * Trying some other modeels for improved results
    
    * Changing model used, extending tokens generated, and adding some logging
    
    * Adding Kafka support and email processing downstream from API
    
    * Notebook reads all emails from kafka
    
    * A notebook for exploring the creation of cypher by extracting from text
    
    * Updating to provide a source of truth to compare to
    
    * Updating notebook, was incorreect
    
    * Updating a notebook and some utils to explore KG creation
    
    * Removing warnings
    
    * Adding support for attachments and reading ics files
    
    * Updating processor to receive events and store to weaviate
    
    * Better gitignore
    
    * Clearing some cache from gitignore issues'
    
    * Dealing with missing description
    
    * Changes for neo4j informed RAG, some refactors
    
    * APOC plugin is required on o4j
    
    * Some changes to generalize the LLM implementation through the API and set temp to 0 globally
    
    * fixing a source of error in parsing bad attachment data
    
    * Poetry changes for new functionality
    
    * Switched to using groq
    
    * removing a key
    
    ---------
    
    Co-authored-by: Keith Nordstrom <keith@timeli.io>

---
+++ tests/library/test_neo4j.py
@@ -0,0 +1,66 @@
+import neo4j.time
+import pytz
+
+
+import unittest
+import os
+
+from library.neo4j import Neo4j
+
+class TestMessage(unittest.TestCase):
+
+    # Cypher query to create the data in the Neo4j database:
+    #
+    # CREATE (p1:Person {name: 'Keith Nordstrom',email:'keith@cognimate.ai'})
+    # CREATE (p2:Person {name: 'Mithali Shashidhar',email:'mithali@cognimate.ai'})
+    # CREATE (p3:Person {name: 'Prakash Aditham',email:'prakash@cognimate.ai'})
+    # CREATE (p4:Person {name: 'Pradeep Javangula',email:'pradeep@cognimate.ai'})
+    # CREATE (e1:Event {start:datetime('2024-05-06T17:00:00-06:00'), end:datetime('2024-05-06T17:30:00-06:00'),name:'Kickoff Meeting', description:'Getting everyone together to discuss the sofia project'})
+    # CREATE (e2:Event {start:datetime('2024-05-08T17:00:00-06:00'), end:datetime('2024-05-08T17:30:00-06:00'),name:'Follow Up Meeting', description:"We need to meet to answer any questions anyone has about the pitch deck"})
+    # CREATE (p1)-[:ATTENDS {status:"Unknown"}]->(e1)
+    # CREATE (e1)-[:INVITED]->(p1)
+    # CREATE (p2)-[:ATTENDS {status:"Declined"}]->(e1)
+    # CREATE (e1)-[:INVITED]->(p2)
+    # CREATE (p3)-[:ATTENDS {status:"Accepted"}]->(e1)
+    # CREATE (e1)-[:INVITED]->(p3)
+    # CREATE (p4)-[:ATTENDS {status:"Declined"}]->(e1)
+    # CREATE (e1)-[:INVITED]->(p4)
+    # CREATE (p1)-[:ATTENDS {status:"Accepted"}]->(e2)
+    # CREATE (e2)-[:INVITED]->(p1)
+    # CREATE (p2)-[:ATTENDS {status:"Accepted"}]->(e2)
+    # CREATE (e2)-[:INVITED]->(p2)
+    # CREATE (p3)-[:ATTENDS {status:"Accepted"}]->(e2)
+    # CREATE (e2)-[:INVITED]->(p3)
+    # CREATE (p4)-[:ATTENDS {status:"Accepted"}]->(e2)
+    # CREATE (e2)-[:INVITED]->(p4)
+    
+    data = [{'person.name': 'Keith Nordstrom', 'person.email':'keith@cognimate.ai', 'event.name': 'Follow Up Meeting', 'event.description': 'We need to meet to answer any questions anyone has about the pitch deck', 'event.start': neo4j.time.DateTime(2024, 5, 8, 17, 0, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'event.end': neo4j.time.DateTime(2024, 5, 8, 17, 30, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'invite.status': 'Accepted', 'attendee.name': 'Mithali Shashindar', 'attendee.email': 'mithali@cognimate.ai', 'attending.status': 'Accepted'}, 
+        {'person.name': 'Keith Nordstrom', 'person.email':'keith@cognimate.ai', 'person.email':'keith@cognimate.ai', 'event.name': 'Follow Up Meeting', 'event.description': 'We need to meet to answer any questions anyone has about the pitch deck', 'event.start': neo4j.time.DateTime(2024, 5, 8, 17, 0, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'event.end': neo4j.time.DateTime(2024, 5, 8, 17, 30, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'invite.status': 'Accepted', 'attendee.name': 'Keith Nordstrom', 'attendee.email': 'keith@cognimate.ai', 'attending.status': 'Accepted'}, 
+        {'person.name': 'Keith Nordstrom', 'person.email':'keith@cognimate.ai', 'event.name': 'Follow Up Meeting', 'event.description': 'We need to meet to answer any questions anyone has about the pitch deck', 'event.start': neo4j.time.DateTime(2024, 5, 8, 17, 0, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'event.end': neo4j.time.DateTime(2024, 5, 8, 17, 30, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'invite.status': 'Accepted', 'attendee.name': 'Pradeep Javangula', 'attendee.email': 'pradeep@cognimate.ai', 'attending.status': 'Accepted'}, 
+        {'person.name': 'Keith Nordstrom', 'person.email':'keith@cognimate.ai', 'event.name': 'Kickoff Meeting', 'event.description': 'Getting everyone together to discuss the sofia project', 'event.start': neo4j.time.DateTime(2024, 5, 6, 17, 0, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'event.end': neo4j.time.DateTime(2024, 5, 6, 17, 30, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'invite.status': 'Unknown', 'attendee.name': 'Mithali Shashindar', 'attendee.email': 'mithali@cognimate.ai', 'attending.status': 'Declined'}, 
+        {'person.name': 'Keith Nordstrom', 'person.email':'keith@cognimate.ai', 'event.name': 'Kickoff Meeting', 'event.description': 'Getting everyone together to discuss the sofia project', 'event.start': neo4j.time.DateTime(2024, 5, 6, 17, 0, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'event.end': neo4j.time.DateTime(2024, 5, 6, 17, 30, 0, 0, tzinfo=pytz.FixedOffset(-360)), 'invite.status': 'Unknown', 'attendee.name': 'Keith Nordstrom', 'attendee.email': 'keith@cognimate.ai', 'attending.status': 'Unknown'}]
+
+    def test_collate_schedule_response(self):
+        response = Neo4j.collate_schedule_response(self.data)
+        assert len(response) == 2
+        assert response[0]['person.name'] == 'Keith Nordstrom'
+        assert response[0]['event.name'] == 'Kickoff Meeting'
+        assert response[0]['event.description'] == 'Getting everyone together to discuss the sofia project'
+        assert response[0]['event.start'] == '2024-05-06T17:00:00.000000000-06:00'
+        assert response[0]['event.end'] == '2024-05-06T17:30:00.000000000-06:00'
+        assert response[0]['invite.status'] == 'Unknown'
+        assert len(response[0]['attendees']) == 1
+        assert response[0]['attendees'][0]['name'] == 'Mithali Shashindar'
+        assert response[0]['attendees'][0]['email'] == 'mithali@cognimate.ai'
+
+        assert response[1]['person.name'] == 'Keith Nordstrom'
+        assert response[1]['event.name'] == 'Follow Up Meeting'
+        assert response[1]['event.description'] == 'We need to meet to answer any questions anyone has about the pitch deck'
+        assert response[1]['event.start'] == '2024-05-08T17:00:00.000000000-06:00'
+        assert response[1]['event.end'] == '2024-05-08T17:30:00.000000000-06:00'
+        assert response[1]['invite.status'] == 'Accepted'
+        assert len(response[1]['attendees']) == 2
+        assert response[1]['attendees'][0]['name'] == 'Mithali Shashindar'
+        assert response[1]['attendees'][0]['email'] == 'mithali@cognimate.ai'
+        assert response[1]['attendees'][1]['name'] == 'Pradeep Javangula'
+        assert response[1]['attendees'][1]['email'] == 'pradeep@cognimate.ai'
