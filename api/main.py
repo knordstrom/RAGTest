4bb3235a8156b1d0bce359e096db3d7417c6d9a6
Mithali <mshash@uw.edu>
2024-05-12 15:36:37 -0700

    changes made for calendar api to kg

--- api/main.py
+++ api/main.py
@@ -1,12 +1,20 @@
 import json
 from os import abort
 import flask
-import context
+# import context
 from library.gmail import Gmail, GmailLogic
 from library.llm import LLM, Wizard, Hermes, Falcon, Mini, MistralInstruct, MistralOrca
 import importlib as i
 import library.weaviate as weaviate
 from kafka import KafkaProducer
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 class APISupport:
 
@@ -44,6 +52,33 @@ class APISupport:
         print("Wrote " + str(count) + " emails to Kafka")
 
     @staticmethod
+    def write_to_kafka_cal(events: dict) -> None:
+        producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092', 
+                                 api_version="7.3.2", 
+                                 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        count = 0
+        # for person in people:
+        #     if person == None:
+        #         print("There are no attendees")
+        #         continue
+        #     producer.send('calendar', value = person)
+        
+        for event in events:
+            count += 1
+            if event == None:
+                print("There are no events")
+                continue
+            producer.send('calendar', value = event)
+
+        # for attendance in attendances:
+        #     if attendance == None:
+        #         print("There are no relationships")
+        #         continue
+        #     producer.send('calendar', value = attendance)
+        producer.flush()
+        print("Wrote " + str(count) + " calendar events to Kafka")
+
+    @staticmethod
     def get_list_from_string(self, string: str) -> list:
         if string == None:
             return []
@@ -75,5 +110,54 @@ def email() -> str:
         flask.abort(404)
         return "No email provided"
 
+@app.route('/calendar', methods=['GET'])
+def calendar() -> str:
+    # email = flask.request.args.get('e')
+    # count = flask.request.args.get('n', None, int)
+    SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "/Users/mithalishashidhar/Downloads/cognimate/RAGTest/api/credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=3000)
+            # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("calendar", "v3", credentials=creds)
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=2,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+        print("events: ", events)
+        if not events:
+            print("No upcoming events found.")
+            return
+        APISupport.write_to_kafka_cal(events)
+        return events
+            
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return "error"
+       
 if __name__ == '__main__':
     app.run()
--- processor/main.py
+++ processor/main.py
@@ -4,6 +4,7 @@ import os
 import library.weaviate as weaviate
 import traceback
 import warnings
+from py2neo import Graph, Node, Relationship
 
 warnings.simplefilter("ignore", ResourceWarning)
 
@@ -24,7 +25,8 @@ def write_to_vdb(mapped: list) -> None:
                     if email['body'] == None or email['body'] == '':
                         email['body'] = email['subject']
                     w.upsertChunkedText(email, weaviate.WeaviateSchemas.EMAIL_TEXT, weaviate.WeaviateSchemas.EMAIL, 'body')
-
+                    print("upsertedchunked part is complete")
+                    # print("events: ", event)
                     for event in events:
                         print("Upserting event " + str(event) + " on from " + str(email['from']))
                         if event.get('description') == None or event.get('description') == '':
@@ -36,6 +38,58 @@ def write_to_vdb(mapped: list) -> None:
             if w is not None:
                 w.close()
 
+def write_to_neo4j(events) -> None:
+        db = os.getenv("NEO4J_DB_HOST", "127.0.0.1")
+        db_port = os.getenv("NEO4J_DB_PORT", "8080")
+        # url = "neo4j://localhost:7687"
+        url = "neo4j://" + db + ":" + db_port
+        username = "neo4j"
+        password = "password"
+        graph = Graph(url, auth=(username, password))
+        print("Writing to Neo4j at " + ":" + url + " ... " + str(len(events)))
+        person_list = []
+        events_list = []
+        attendance_list = []
+        try:
+            for record in events:
+                event = record.value
+                print("event: ", event)
+                start = event["start"].get("dateTime", event["start"].get("date"))
+                end = event["end"].get("dateTime", event["end"].get("date"))
+                eventName = event["summary"]
+                event_dict = {"id": eventName, "starttime": start, "endtime": end}
+                events_list.append(event_dict)
+                attendees = event["attendees"]
+                for item in attendees:
+                    attendee = {"id": item["email"]}
+                    rel_dict = {"person_id": item["email"], "event_id": event_dict["id"]}
+                    attendance_list.append(rel_dict)
+                    person_list.append(attendee)
+            # Creating and merging Person nodes
+            for person in person_list:
+                node = Node("Person", id=person['id'])
+                graph.merge(node, "Person", "id")
+
+
+            # Creating and merging Event nodes
+            for event in events_list:
+                node = Node("Event", id=event['id'], starttime=event['starttime'], endtime=event['endtime'])
+                graph.merge(node, "Event", "id")
+
+            # Creating ATTENDS relationships
+            for attend in attendance_list:
+                person_node = graph.nodes.match("Person", id=attend['person_id']).first()
+                event_node = graph.nodes.match("Event", id=attend['event_id']).first()
+                rel = Relationship(person_node, "ATTENDS", event_node)
+                graph.merge(rel)
+
+            print("Data has been successfully added to the graph!")
+                
+
+        except TypeError as error:
+            print(f"An error occurred: {error}")
+
+
 def start():
     kafka = os.getenv("KAFKA_BROKER", "127.0.0.1:9092")
     topic = os.getenv("KAFKA_TOPIC", "emails")
@@ -43,7 +97,8 @@ def start():
     try:
         consumer = KafkaConsumer(bootstrap_servers=kafka, 
                                 group_id='processor',
-                                value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+                                value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+                                api_version="7.3.2")
         consumer.subscribe(topics=[topic])
         print("Subscribed to " + topic + ", waiting for messages...")
 
@@ -60,6 +115,7 @@ def start():
             print("Tick")
             try:
                 message = consumer.poll(timeout_ms=2000)
+                print("message: ", message)
             except Exception as e:
                 print("Error: " + str(e))
                 continue
@@ -80,5 +136,55 @@ def start():
         print("Closing consumer")
         consumer.close()
 
+
+def start_kafka_calendar():
+    kafka = os.getenv("KAFKA_BROKER", "127.0.0.1:9092")
+    topic = os.getenv("KAFKA_TOPIC", "calendar")
+    print("Starting processor at " + kafka + " on topic " + topic + " ...")
+    try:
+        consumer = KafkaConsumer(bootstrap_servers=kafka, 
+                                group_id='processor2',
+                                value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+                                api_version="7.3.2")
+        consumer.subscribe(topics=[topic])
+        print("Subscribed to " + topic + ", waiting for messages...")
+
+        count = 0
+
+        key: TopicPartition = TopicPartition(topic=topic, partition=0)
+        partitions = None
+        message = None
+        while partitions == None or len(partitions) == 0:
+            print("IM here")
+            partitions = consumer.partitions_for_topic(topic)
+            print("now here: ", partitions)
+            print("Waiting for partitions... have " + str(partitions))
+            
+        while True:
+            print("Tick")
+            try:
+                message = consumer.poll(timeout_ms=2000)
+            except Exception as e:
+                print("Error: " + str(e))
+                continue
+
+            if message is None or message == {}:  
+                continue
+            else:
+                count += 1
+                print("Received message " + str(count) + ":" + str(message))
+                print()
+
+                write_to_neo4j(message[key])
+                print(" ... written to VDB")
+            
+            consumer.commit()
+            
+    finally:
+        print("Closing consumer")
+        consumer.close()
+
+
 if __name__ == '__main__':
     start()
+    start_kafka_calendar()
