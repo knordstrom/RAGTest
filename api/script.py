655906d714b7f515b1537f9fe1b0162b0f0bf8a6
Mithali <mshash@uw.edu>
2024-08-01 11:43:20 -0700

    made changes based on pr comments

--- api/script.py
+++ api/script.py
@@ -26,27 +26,22 @@ default_destination = {'destination': '/data/script'}
 @route.get('/data/script')
 def slack() -> str:
     """This script is used to ingest demo information from a script to automate data ingestion"""
-    try:
-        with open(root_path + '/../demo_script/calendar.json', 'r') as cal_file:
-            cal_data = json.load(cal_file)
-        APISupport.write_cal_to_kafka(cal_data, DataSources.GOOGLE)
+    with open(root_path + '/../demo_script/calendar.json', 'r') as cal_file:
+        cal_data = json.load(cal_file)
+    APISupport.write_cal_to_kafka(cal_data, DataSources.GOOGLE)
 
-        with open(root_path + '/../demo_script/email.json', 'r') as email_file:
-            email_data = json.load(email_file)
-        adapter = TypeAdapter(List[Message])
-        emails = adapter.validate_python(email_data)
-        APISupport.write_emails_to_kafka(emails, DataSources.GOOGLE)
+    with open(root_path + '/../demo_script/email.json', 'r') as email_file:
+        email_data = json.load(email_file)
+    adapter = TypeAdapter(List[Message])
+    emails = adapter.validate_python(email_data)
+    APISupport.write_emails_to_kafka(emails, DataSources.GOOGLE)
 
-        with open(root_path + '/../demo_script/documents.json', 'r') as docs_file:
-            docs_data = json.load(docs_file)
-        APISupport.write_docs_to_kafka([x for x in docs_data.values()], DataSources.GOOGLE)
+    with open(root_path + '/../demo_script/documents.json', 'r') as docs_file:
+        docs_data = json.load(docs_file)
+    APISupport.write_docs_to_kafka([x for x in docs_data.values()], DataSources.GOOGLE)
 
-        with open(root_path + '/../demo_script/slack.json', 'r') as slack_file:
-            conversations = json.load(slack_file)
-        APISupport.write_slack_to_kafka(conversations)
-    
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        APISupport.error_response(500, f"An HTTP error occurred '{error}'")
+    with open(root_path + '/../demo_script/slack.json', 'r') as slack_file:
+        conversations = json.load(slack_file)
+    APISupport.write_slack_to_kafka(conversations)
     return "successfully ingested!"
 
