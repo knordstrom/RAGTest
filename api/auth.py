80af3d8af3e5f01a611d2eee12c227f11f7aa70c
Keith Nordstrom <keith@timeli.io>
2024-09-09 19:47:34 -0600

    Some changes to allow things to work in the cloud

--- api/auth.py
+++ api/auth.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import json
+from typing import Optional
+from globals import Globals
 from library.models.api_models import ApiResponse, LoginRequest, TokenResponse
 from fastapi import APIRouter
 
@@ -11,3 +15,11 @@ async def authenticate(form: LoginRequest) -> ApiResponse[TokenResponse]:
     result: TokenResponse = AuthManager().authenticate(form.email, form.password)
     return ApiResponse.create(result)
     
+@route.post('/auth/creds')
+async def credentials(target: str, token: str, refresh_token: str, expiry: datetime, client_id: Optional[str] = None, 
+                      client_secret: Optional[str] = None, scopes: Optional[str]= None) -> ApiResponse[str]:
+    """Store credentials for a user"""
+    with open(Globals().root_resource(target + ".json"), "w") as f:
+        f.write(json.dumps({"token": token, "refresh_token": refresh_token, "expiry": expiry.isoformat(), 
+                            "client_id": client_id, "client_secret": client_secret, "scopes": scopes.split(",")}))
+    return ApiResponse.create("True")
